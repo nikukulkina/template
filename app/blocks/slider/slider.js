be9f401d90919b488da7facd27bfcc3ca4614b19
nikukulkina <nikukulkina@gmail.com>
2019-09-17 18:30:44 +0300

    стрелки для слайдера

--- app/blocks/slider/slider.js
+++ app/blocks/slider/slider.js
@@ -4,9 +4,10 @@ export const initialSlider = () => {
 	const slider = new Swiper('.swiper-container', {
 		slidesPerView: 2,
 		spaceBetween: 30,
+		loop: true,
 		navigation: {
-			nextEl: '.swiper-button-prev',
-			prevEl: '.swiper-button-next'
+			nextEl: '.slider__btn-next',
+			prevEl: '.slider__btn-prev'
 		}
 	});
 };
