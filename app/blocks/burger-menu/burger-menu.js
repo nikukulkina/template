93d61429f18be606797b6755141f5ee26e76f451
nikukulkina <nikukulkina@gmail.com>
2019-09-26 00:35:49 +0300

    кнопка бургер-меню
    кнопка закреплена, добавлен фон при скролле, чтобы не сливалась со страницей

--- app/blocks/burger-menu/burger-menu.js
+++ app/blocks/burger-menu/burger-menu.js
@@ -1,5 +1,13 @@
 export const burger = () => {
-	const menuButton = document.querySelector('.burger-menu');
+	const menuButton = document.querySelector('.burger-menu__wrap');
+	window.addEventListener('scroll', function () {
+		const menu = document.querySelector('.burger-menu');
+		if (pageYOffset > 140) {
+			menu.classList.add('burger-menu_scrolled');
+		}else {
+			menu.classList.remove('burger-menu_scrolled');
+		}
+	});
 	menuButton.addEventListener('click', function () {
 		const lines = menuButton.children;
 		for (const item of lines) {
