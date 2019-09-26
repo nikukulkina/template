fbc0c95748f419c26658cbffe81a1b18931819a8
nikukulkina <nikukulkina@gmail.com>
2019-09-26 16:52:43 +0300

    Мобильное меню

--- app/blocks/burger-menu/burger-menu.js
+++ app/blocks/burger-menu/burger-menu.js
@@ -1,5 +1,8 @@
 export const burger = () => {
 	const menuButton = document.querySelector('.burger-menu__wrap');
+	const menuLinks = document.querySelector('.burger-menu__links');
+	const menuLink = document.querySelectorAll('.burger-menu__link');
+	const overlay = document.querySelector('.page-main__overlay');
 	window.addEventListener('scroll', function () {
 		const menu = document.querySelector('.burger-menu');
 		if (pageYOffset > 140) {
@@ -8,10 +11,41 @@ export const burger = () => {
 			menu.classList.remove('burger-menu_scrolled');
 		}
 	});
-	menuButton.addEventListener('click', function () {
+	const menuBtnChange = function () {
 		const lines = menuButton.children;
 		for (const item of lines) {
 			item.classList.toggle('burger-menu__line_active');
 		}
+	};
+	const menuOverlayChange = function () {
+		const body = document.querySelector('.page');
+		overlay.classList.toggle('page-main__overlay_active');
+		if (overlay.classList.contains('page-main__overlay_active')) {
+			body.style.overflow = 'hidden';
+		}else {
+			body.style.overflow = 'auto';
+		}
+	};
+	const menuItemsChange = function () {
+		menuLinks.classList.toggle('burger-menu__links_active');
+	};
+	menuButton.addEventListener('click', function (e) {
+		e.preventDefault();
+		menuBtnChange();
+		menuOverlayChange();
+		menuItemsChange();
 	});
+	overlay.addEventListener('click', function () {
+		menuBtnChange();
+		menuOverlayChange();
+		menuItemsChange();
+	});
+
+	for (let i = 0; i < menuLink.length; i++){
+		menuLink[i].addEventListener('click', function () {
+			menuBtnChange();
+			menuOverlayChange();
+			menuItemsChange();
+		});
+	}
 };
